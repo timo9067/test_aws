b5081b04caf1c97e97572a2a1cb28451046cc96b
Timur <timur@shakirov.com.ua>
2023-10-13 15:23:11 +0200

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+function changeTheme(theme) {
+    if (theme == 'dark'){
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
